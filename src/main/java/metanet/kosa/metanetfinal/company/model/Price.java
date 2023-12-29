80b529dd210e6f83125076d9b5c6b19020361630
SHIN-HANBEEN <shb0801@naver.com>
2023-12-29 20:11:09 +0900

    feat: sql.txt 추가

---
+++ src/main/java/metanet/kosa/metanetfinal/company/model/Price.java
@@ -0,0 +1,7 @@
+package metanet.kosa.metanetfinal.company.model;
+
+public class Price {
+	private static float adult = 0.0f;
+	private static float student = 0.0f;
+	private static float child = 0.0f;
+}
