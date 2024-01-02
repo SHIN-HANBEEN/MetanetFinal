e1791d22638de21e16b4f17d9addd4b9f9dceee4
SHIN-HANBEEN <shb0801@naver.com>
2024-01-02 18:03:50 +0900

    Feat: AOP 로깅 적용 완료

---
+++ src/main/java/metanet/kosa/metanetfinal/common/LogAspect.java
@@ -0,0 +1,31 @@
+package metanet.kosa.metanetfinal.common;
+
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+	@Before("execution(* metanet.kosa.metanetfinal..*.*(..))")
+	public void beforeLog(JoinPoint joinPoint) {
+		Signature signature = joinPoint.getSignature();
+		String methodName = signature.getName();
+		log.info("[[[AOP-before Log]]]-{}", methodName);
+	}
+	
+	@AfterThrowing(pointcut = "* metanet.kosa.metanetfinal..*.*(..)", throwing = "exception")
+	public void afterThrowingLog(JoinPoint joinPoint, Exception exception) {
+		Signature signature = joinPoint.getSignature();
+		String methodName = signature.getName();
+		log.info("[[AOP-after throwing Log]]-{}, ex: {}", methodName, exception.getMessage());
+	}
+}
