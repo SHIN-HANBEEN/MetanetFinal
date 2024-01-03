d602dc4014695d9d0cdb930dafd70771074ccaab
wans <rladhkstmd10@hanmail.net>
2024-01-04 08:49:37 +0900

    feat: 결제 Script 수정, 결제코드 비동기 요청, WebMVC CORS 임시 해결 Request를 MAP으로 바꾸기 위한 JSON 라이브러리 추가

--- src/main/java/metanet/kosa/metanetfinal/config/WebMvcConfig.java
+++ src/main/java/metanet/kosa/metanetfinal/config/WebMvcConfig.java
@@ -2,9 +2,18 @@ package metanet.kosa.metanetfinal.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAspectJAutoProxy
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addMapping("/**")
+        .allowedMethods("*");
+	}
 	
 }
