7720d3941acaa343a7e9a87334e21f0dc3724f78
wans <rladhkstmd10@hanmail.net>
2024-01-07 18:07:19 +0900

    feat: resource에 static/assets 추가

--- src/main/java/metanet/kosa/metanetfinal/reservation/controller/reservationPageController.java
+++ src/main/java/metanet/kosa/metanetfinal/reservation/controller/reservationPageController.java
@@ -1,8 +1,24 @@
 package metanet.kosa.metanetfinal.reservation.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class reservationPageController {
+	
+	@GetMapping("/reservation/seats-selection")
+	public String seatsSelectPage() {
+		
+		return "/reservation/seat_reservation";
+	}
 
+	@GetMapping("/reservation/payment")
+	public String paymentPage() {
+
+		return "/reservation/pay";
+	}
 }
