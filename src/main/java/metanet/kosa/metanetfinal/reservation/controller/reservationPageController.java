57813f5994b3880cbcdc2b30f23be9ad26e76d6c
wans <rladhkstmd10@hanmail.net>
2024-01-07 22:00:56 +0900

    feat: 좌석선택 전체 완료 결제 페이지로 넘어가자

--- src/main/java/metanet/kosa/metanetfinal/reservation/controller/reservationPageController.java
+++ src/main/java/metanet/kosa/metanetfinal/reservation/controller/reservationPageController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class reservationPageController {
@@ -16,9 +17,14 @@ public class reservationPageController {
 		return "/reservation/seat_reservation";
 	}
 
+	@PostMapping("/reservation/payment")
+	public Map<String, Object> paymentPage(@RequestBody Map<String, Object> request) {
+		System.out.println(request);
+		return request;
+	}
 	@GetMapping("/reservation/payment")
-	public String paymentPage() {
-
+	public String paymentPage1() {
+		
 		return "/reservation/pay";
 	}
 }
