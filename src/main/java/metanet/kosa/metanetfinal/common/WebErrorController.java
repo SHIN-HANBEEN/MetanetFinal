3bbd091db521cabfeacc1ce02cf5d3178dc03164
SHIN-HANBEEN <shb0801@naver.com>
2024-01-17 11:55:47 +0900

    Feat: error controller 수정완료

--- src/main/java/metanet/kosa/metanetfinal/common/WebErrorController.java
+++ src/main/java/metanet/kosa/metanetfinal/common/WebErrorController.java
@@ -37,13 +37,6 @@ public class WebErrorController implements ErrorController {
             }else if (statusCode == HttpStatus.FORBIDDEN.value()) {
                 return ERROR_403_PAGE_PATH;
             }
-<<<<<<< HEAD
-
-=======
-            else if (statusCode == HttpStatus.valueOf(403).value()) {
-            	return ERROR_403_PAGE_PATH;
-			}
->>>>>>> 94127e10193d823be32ac8cc065d03ccc80696c3
         }
         return ERROR_ETC_PAGE_PATH;
     }
